33bf84f4ab29618204b0a1399897fa480e5f1738
monique <michael.spengler@fh-hwz.ch>
2024-01-02 15:50:07 +0100

    update deps

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export * from "https://deno.land/x/bollinger_bands@v0.3.0/mod.ts"
-export * from "https://deno.land/x/sleepv1.3.0/mod.ts"
+export * from "https://deno.land/x/sleep@v1.3.0/mod.ts"
 export * from "https://deno.land/x/log@v1.1.1/mod.ts"
